ba4314932fe59f9991861a91b71c5ed5f3e9aaaa
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-09-28 16:47:20 -0300

    fix meteor

--- galaxy-fast-build/scripts/install_meteor.sh
+++ galaxy-fast-build/scripts/install_meteor.sh
@@ -5,16 +5,9 @@ set -e
 # download installer script
 curl -v https://install.meteor.com -o /tmp/install_meteor.sh
 
-# read in the release version in the app
-METEOR_VERSION=$(head $APP_SOURCE_FOLDER/.meteor/release | cut -d "@" -f 2)
-
-# set the release version in the install script
-sed -i.bak "s/RELEASE=.*/RELEASE=\"$METEOR_VERSION\"/g" /tmp/install_meteor.sh
-
-# replace tar command with bsdtar in the install script (bsdtar -xf "$TARBALL_FILE" -C "$INSTALL_TMPDIR")
 # https://github.com/jshimko/meteor-launchpad/issues/39
 sed -i.bak "s/tar -xzf.*/tar -xf \"\$TARBALL_FILE\" -C \"\$INSTALL_TMPDIR\"/g" /tmp/install_meteor.sh
 
-# install
-printf "\n[-] Installing Meteor $METEOR_VERSION...\n\n"
+# Install Meteor
+printf "\n[-] Installing the latest version of Meteor...\n\n"
 sh /tmp/install_meteor.sh
